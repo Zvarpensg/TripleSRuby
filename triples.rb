83ca5dfac1620e6c744b61a1b9fb83abb9f86e0a
SkySom <skylar.som@gmail.com>
2015-02-07 01:12:14 -0500

    Added json file for bot.

--- triples.rb
+++ triples.rb
@@ -1,16 +1,22 @@
 require 'cinch'
+require 'json'
 
 # require all plugins in plugins/
 Dir[File.dirname(__FILE__) + '/plugins/*.rb'].each {|file| require file }
 
-bot = Cinch::Bot.new do	
-	configure do |c|
-		c.server = "irc.freenode.net"
-		c.nick = "TripleS"
-		c.channels = [""]
+bot = Cinch::Bot.new do
+	if(File.exists?("./bot.json")) then
+		file = File.open("./bot.json").read
+		botJSON = JSON.parse file
 
-		c.plugins.plugins = [Hello, FuckYeah, FML]
-		c.plugins.prefix = /^(!|#{c.nick}[,: ]*)/
+		configure do |c|
+			c.nick = botJSON["nick"]
+			c.server = botJSON["server"]
+			c.channels = botJSON["channels"]
+
+			c.plugins.plugins = [Hello, FuckYeah, FML]
+			c.plugins.prefix = /^(!|#{c.nick}[,: ]*)/
+		end
 	end
 end
 
