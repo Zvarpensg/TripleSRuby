02772fec540d08b76b4607b475de606b30938acc
cmhull42 <cmhull42@gmail.com>
2015-05-08 18:33:50 -0500

    Use permissions with CatFace plugin

--- plugins/catface.rb
+++ plugins/catface.rb
@@ -2,10 +2,12 @@
 # encoding: utf-8
 class CatFace
   include Cinch::Plugin
+  include Authentication
 
   match "catface"
 
   def execute(m)
+    not_banned?(@bot, m) or return
     m.channel.send("ฅ^•ﻌ•^ฅ")
   end
 end
