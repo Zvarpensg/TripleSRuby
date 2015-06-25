2898a98f099b8074b897f221de61fd40a22495e2
Graham Watt <gmwatt@gmail.com>
2015-06-25 15:59:30 -0400

    Add anagram

---
+++ plugins/anagram.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class Anagram
+	include Cinch::Plugin
+	include Authentication
+	match /anagram (.+)/i
+
+	def execute(m, arg)
+		puts "Anagram?"
+		not_banned?(@bot, m) or return
+
+		response = Net::HTTP.post_form(
+			URI.parse('http://www.sternestmeanings.com/say.json'), 'msg' => arg)
+		j = JSON.parse(response.body)
+		if j.class == Array
+			m.channel.send "No anagram for you"
+		else
+			m.channel.send "\"#{arg}\" => \"#{j['message']['response']}\""
+		end
+	end
+end
+		
