de7c423c0bed76b88405ffecb455c638887ac938
cmhull42 <cmhull42@gmail.com>
2015-08-03 20:57:28 -0500

    Add decide plugin

---
+++ plugins/decide.rb
@@ -0,0 +1,20 @@
+
+class Decide
+	include Cinch::Plugin
+	include Authentication
+	
+	match /decide (.+)/, method: :decide
+	
+	def decide(m, args)
+		not_banned?(@bot, m) or return
+		
+		if args.include? ","
+			args = args.split ","
+		elsif args.include? " "
+			args = args.split " "
+		end
+		
+		gen = Random.new
+		m.channel.send "#{m.user.nick}: you should #{args[gen.rand(args.size)].strip}"
+	end
+end
