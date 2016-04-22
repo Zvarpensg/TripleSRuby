19d70904459978bd4b03e977c6b62b096f42cf57
Graham Watt <gmwatt@gmail.com>
2016-04-22 01:50:29 -0400

    Split up getting frogtips.
    Use a separate method for getting a specific frogtip and getting a
    random frogtip. Also, use a 3rd method for the actual HTTP request.
    Also, I hate ruby.

--- plugins/frogtips.rb
+++ plugins/frogtips.rb
@@ -4,23 +4,35 @@ require 'json'
 class FrogTips
 	include Cinch::Plugin
 	include Authentication
-	match /frogtip (.+)/i
-	
-	def execute(m, arg)
-		not_banned(@bot, m) or return
 
-		uri = 'http://frog.tips/api/1/tips/'
-
-		if arg != ''
-			uri += arg
-		end
-
-		resp = Net::HTTP.get_response(uri)
+	def make_request(m, arg = '')
+		url = "http://frog.tips/api/1/tips/#{arg}"
+		resp = Net::HTTP.get_response(URI(url))
 		if resp.code_type == Net::HTTPOK
-			m.channel.send resp.body['tips'][0]['tip']
+			return JSON.parse(resp.body)
 		else
-			m.channel.send 'Error fetching frog tip. =('
+			m.channel.send 'Error fetching tip. =('
+			return nil
 		end
-		
+	end
+
+	match /frogtip\s*$/i, method: :random_tip
+	def random_tip(m)
+		not_banned?(@bot, m) or return
+
+		resp = make_request(m)
+		if resp != nil
+			m.channel.send resp['tips'][0]['tip']
+		end
+	end
+
+	match /frogtip ([0-9]+)/i, method: :specific_tip
+	def specific_tip(m, tipno)
+		not_banned?(@bot, m)
+
+		resp = make_request(m, tipno)
+		if resp != nil
+			m.channel.send resp['tip']
+		end
 	end
 end
